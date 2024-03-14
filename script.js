abca5b21609e413d98b69cb8fb686bac5ed7dbc2
kurtCoding <kjulien7790@gmail.com>
2024-03-13 21:20:52 -0400

    added javascript

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-console.log("Hello World");
+const form = document.querySelector("form");
+
+function addDeleteToButton(bttn) {
+    bttn.addEventListener("click", event => {
+        event.target.parentNode.remove()
+    })
+}
+
+const buttons = document.querySelectorAll(".to-dos__delete-button");
+
+for (let button of buttons) {
+    addDeleteToButton(button);
+}
+
+form.addEventListener("submit", event => {
+    event.preventDefault();
+    const todoText = form.toDo.value;
+
+    const todoLi = document.createElement("li");
+
+    todoLi.innerText = todoText;
+
+    const button = document.createElement("button");
+
+    todoLi.appendChild(button);
+
+    //Add functionality to delete
+
+    document.querySelector("ul").appendChild(todoLi);
+
+    todoLi.classList.add("to-dos__list-item");
+    button.classList.add("to-dos__delete-button");
+    button.innerText = "Delete";
+    
+    form.reset();
+})
